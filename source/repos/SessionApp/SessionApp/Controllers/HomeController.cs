c4315e9293c8e831b063122f303bce3fca65827a
Volkan Sen <volkanssenn@gmail.com>
2023-09-22 11:39:13 +0300

    test

---
+++ source/repos/SessionApp/SessionApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SessionApp.Models;
+using System.Diagnostics;
+
+namespace SessionApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Oturumu etkinleştir
+            HttpContext.Session.SetString("StudentName", "John");
+            HttpContext.Session.SetInt32("StudentId", 50);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
